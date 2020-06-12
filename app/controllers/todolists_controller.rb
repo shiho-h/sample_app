b94877f81890ef223f3cdab69533b3d8c44273df
shiho-h <sisopiku@gmail.com>
2020-06-12 20:25:49 +0900

    finish3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,5 +1,6 @@
 class TodolistsController < ApplicationController
   def new
+  	# Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
   	@list = List.new
   end
 
@@ -7,9 +8,9 @@ class TodolistsController < ApplicationController
 
   	list = List.new(list_params)
 
-  	list.save
+  	list.save #　データベースへ保存する
 
-  	redirect_to todolist_path(list.id)
+  	redirect_to todolist_path(list.id) # 詳細画面へリダイレクト
 
 	end
 
@@ -18,12 +19,28 @@ class TodolistsController < ApplicationController
 	end
 
 	def show
-		@list = List.find(params[:id])
+        @list = List.find(params[:id])
+    end
+
+	def edit
+        @list = List.find(params[:id])
+    end
+
+	def update
+		list = List.find(params[:id])
+        list.update(list_params)
+        redirect_to todolist_path(list.id)
 	end
 
-	private
+	def destroy
+		 list = List.find(params[:id]) #データ(レコード)を1件取得
+        list.destroy #データ（レコード）を削除
+        redirect_to todolists_path #List一覧画面へリダイレクト
+	end
 
+
+	private
 	def list_params
-		params.require(:list).permit(:title, :body)
+		params.require(:list).permit(:title, :body, :image)
 	end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1a4ae9b25a2b5a60feee95557a61115524d83bd1ffb802534d3e3dbb4546b40394dfbc3e29ec94053885bc58a6aa27cebad5e83f9a774aeb3793f02e699ed60f'
